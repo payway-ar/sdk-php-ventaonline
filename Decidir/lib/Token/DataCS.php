c0700b7ab27be68879ffa73cfd5953336b3041c4
ealganaraz <ealganaraz@prismamp.com>
2022-03-23 14:44:00 -0300

    mm

--- Decidir/lib/Token/DataCS.php
+++ Decidir/lib/Token/DataCS.php
@@ -5,17 +5,14 @@ class DataCS extends \Decidir\Token\Data{
 
     public function __construct(array $data) {
 
-		$this->setRequiredFields(array(
-			"fraud_detection" => array(
+        $this->setOptionalFields(array(
+            "fraud_detection" => array(
 				"name" => array(
                     "device_unique_identifier" => array(
                         "name"=> "device_unique_identifier"
                     )
                 )
 			),
-		));
-
-        $this->setOptionalFields(array(
             "ip_address" => array(
                 "name" => "ip_address"
             )
