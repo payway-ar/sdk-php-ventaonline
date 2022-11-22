9a0f8df09fa01c863240aa738c11ffa1c8c07f41
ealganaraz <ealganaraz@prismamp.com>
2022-11-22 18:40:36 -0300

    cc

--- Decidir/lib/Checkout/Hash/HashResponse.php
+++ Decidir/lib/Checkout/Hash/HashResponse.php
@@ -1,7 +1,7 @@
 <?php
 namespace Decidir\Checkout\Hash;
 
-class PaymentResponse extends \Decidir\Data\Response {
+class HashResponse extends \Decidir\Data\Response {
 
 	public function __construct(array $data) {
 		parent::__construct($data);
