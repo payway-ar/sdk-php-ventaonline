69f42204db8f7571d0391d80b605fb7ada07914f
ealganaraz <ealganaraz@prismamp.com>
2022-11-22 18:55:10 -0300

    fixing url

--- Decidir/lib/Payment.php
+++ Decidir/lib/Payment.php
@@ -124,7 +124,7 @@ class Payment{
 	public function GenerateLink($data){
 		$jsonData = new \Decidir\Checkout\Hash\Data($data);
 		$data['origin_platform'] = "SDK-PHP";
-		$RESTResponse = $this->serviceREST->post("api/orchestrator/checkout/payments/link", $jsonData->getData());
+		$RESTResponse = $this->serviceREST->post("orchestrator/checkout/payments/link", $jsonData->getData());
 		$ArrayResponse = $this->toArray($RESTResponse);
 		return new \Decidir\Checkout\Hash\HashResponse($ArrayResponse);
 	}
