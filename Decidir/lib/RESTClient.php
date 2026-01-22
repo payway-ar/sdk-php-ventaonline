1f0dd7bc59d539bf716a4bcbd5007abf29d05769
gbangertes-rb <gabriel.bangertes@redb.ee>
2026-01-22 17:18:03 -0300

    Add apikey (healthcheck and cpb endpoints)

--- Decidir/lib/RESTClient.php
+++ Decidir/lib/RESTClient.php
@@ -69,7 +69,7 @@ class RESTClient{
 		$this->action = $action;
 
 		if($action == 'healthcheck' || $action == 'checkout-payment-button/link'){
-			$this->key = "";
+            $this->key = $this->keys_data['private_key'];
 
 		}elseif($action == 'tokens'){
 			$this->key = $this->keys_data['public_key'];
